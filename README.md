950d445dcb4b798752e8327b10a93219d8de5d63
Ribamar FS <ribafs@gmail.com>
2018-07-12 10:41:09 -0300

    Commit da segunda versão

---
+++ README.md
@@ -0,0 +1,2 @@
+# percurso-google
+Módulo para Joomla 2.5 e 3 que mostra um mapa do Google e permite que se entre origem e destino e ele traça o percurso entre a origem e o destino.
---
+++ mod_percurso_google.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @module Percurso
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @author - Ribamar FS - 04/2016
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+?>
+<stong>Este módulo exibe o mapa do Google e traça o percurso no mapa entre dois endereços.</stong>
+<a href="modules/mod_percurso_google/percurso.html" target="_blank">Google Percurso</a>
