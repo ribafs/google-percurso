40628210db72a64fe298790853fc8d5203a386ec
Ribamar FS <ribafs@gmail.com>
2021-06-22 10:43:01 -0300

    Update mod_percurso_google.php

--- mod_percurso_google.php
+++ mod_percurso_google.php
@@ -9,4 +9,4 @@ defined('_JEXEC') or die('Restricted access');
 
 ?>
 <stong>Este módulo exibe o mapa do Google e traça o percurso no mapa entre dois endereços.</stong>
-<a href="modules/mod_percurso_google/percurso.html" target="_blank"><h4>Google Percurso</h4></a>
+<a href="modules/mod_percurso_google/percurso.html" target="_blank"><h4>Acessar</h4></a>
